33f4652c0e286ae9cdc661f29bea2ac5c0440714
Daria Osipova <v-daosip@microsoft.com>
2020-09-09 19:45:04 +0300

    initial commit

---
+++ FastNotes.Resources/ResourceProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace FastNotes.Resources
+{
+    /// <summary>
+    /// Allow access to package resources according with current UI culture.
+    /// </summary>
+    public class ResourceProvider
+    {
+        /// <summary>
+        /// Resource file name.
+        /// </summary>
+        private string resourceFileName;
+
+        private ResourceManager resourceManager;
+
+        public ResourceProvider(string resourceFileName)
+        {
+            this.resourceFileName = resourceFileName;
+        }
+
+        private ResourceManager ResourceManager
+        {
+            get
+            {
+                if (this.resourceManager == null)
+                {
+                    this.resourceManager = new ResourceManager(resourceFileName, typeof(ResourceProvider).Assembly);
+                }
+
+                return this.resourceManager;
+            }
+        }
+
+        public CultureInfo SelectedCulture { get; set; }
+
+        public string GetString(string key)
+        {
+            return resourceManager.GetString(key);
+        }
+    }
+}
